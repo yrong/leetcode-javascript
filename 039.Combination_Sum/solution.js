9665ae8bfac3e68a5862fa8b91bf27c0628a2d9c
yrong <yrong1997@gmail.com>
2019-03-01 16:29:38 +0800

    039

---
+++ 039.Combination_Sum/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    var result=[],temp=[];
+    var find =function(target,temp,index){
+        for(var i=index;i>=0;i--){
+            if(candidates[i]>target){
+                continue;
+            }else if(candidates[i]===target){
+                result.push(temp.concat([candidates[i]]));
+            }else{
+                find(target-candidates[i],temp.concat([candidates[i]]),i);
+            }
+        }
+    }
+    find(target,temp,candidates.length-1);
+    return result;
+};
+
+var result = combinationSum([2,3,6,7],7)
+console.log(result)
